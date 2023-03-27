3feab51572b29b989e29fef8b0648622b5739c5e
Jaroslav Pryjmak <73446025+pryjmi@users.noreply.github.com>
2023-03-27 18:54:10 +0200

    exchange rate fetch (#4)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,63 @@
-from flask import render_template
+from flask import render_template, Flask, jsonify
+from bs4 import BeautifulSoup as bs
+import datetime
+import schedule
+import time
+import requests
+import os
+import json
 from app import app
+
+def get_date_to_use(now=None):
+    if now is None:
+        now = datetime.datetime.now()
+
+    if now.weekday() >= 5:
+        days_to_friday = (now.weekday() - 4) % 7
+        friday = now - datetime.timedelta(days=days_to_friday)
+        date_to_use = friday.date()
+    else:
+        date_to_use = now.date()
+    
+    return date_to_use.strftime("%d.%m.%Y")
+
+def get_exchange_rate(path=None):
+    date_to_use = get_date_to_use()
+
+    if path is None:
+        file_name = "exchange_rates.json"
+        path = f"app/{file_name}"
+
+    if os.path.exists(path):
+        with open(path, "r") as file:
+            exchange_rates = json.load(file)
+    else:
+        exchange_rates = {}
+
+    if date_to_use not in exchange_rates:
+        save_exchange_rate(date_to_use, exchange_rates, path)
+
+
+def save_exchange_rate(date_to_use, exchange_rates, path):
+    url = f"https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt;jsessionid=4809CDD45D1657F6A4F2F9A435E55F13?date={date_to_use}"
+    response = requests.get(url)
+
+    data_lines = response.text.strip().split("\n")[2:]
+    parsed_data = {}
+    for line in data_lines:
+        _, _, amount, code, rate = line.split("|")
+        parsed_data[code] = {
+            'amount': float(amount),
+            'rate': float(rate.replace(",", "."))
+        }
+    exchange_rates[date_to_use] = parsed_data
+
+    with open(path, "w") as file:
+        json.dump(exchange_rates, file)
 
 @app.route("/")
 @app.route("/index")
 def index():
     user = "uživatel"
+    get_exchange_rate()
     return render_template("index.html", user=user)
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,5 +1,8 @@
 import pytest
+import json
+import datetime
 from flask import url_for
+from app.main import  get_date_to_use, get_exchange_rate, save_exchange_rate
 
 def test_first_page(client):
     response = client.get("/")
@@ -10,3 +13,40 @@ def test_homepage(client):
     response = client.get("/index")
     assert response.status_code == 200
     assert b"<title>Bank Home</title>" in response.data
+
+def test_get_date_to_use():
+    # weekend
+    weekend = datetime.datetime(2023, 3, 25) # saturday
+    expected_date_weekend = "24.03.2023" # nearest friday
+    assert get_date_to_use(weekend) == expected_date_weekend
+    # weekday
+    weekday = datetime.datetime(2023, 3, 22) # tuesday
+    expected_date_weekday = "22.03.2023" # same day
+    assert get_date_to_use(weekday) == expected_date_weekday
+
+def test_get_exchange_rate(tmp_path):
+    test_file = tmp_path / "exchange_rates_test.json"
+    get_exchange_rate(test_file)
+    
+    assert test_file.exists()
+    
+    with open(test_file, "r") as file:
+        exchange_rates = json.load(file)
+    
+    date_to_use = get_date_to_use()
+    assert date_to_use in exchange_rates
+
+def test_save_exchange_rate(tmp_path):
+    test_file = tmp_path / "exchange_rates_test.json"
+    test_date = "25.03.2023"
+    test_data = {}
+    
+    save_exchange_rate(test_date, test_data, test_file)
+    assert test_file.exists()
+    
+    with open(test_file, "r") as file:
+        data = json.load(file)
+    assert data
+    
+    assert test_date in data
+    assert data[test_date]
