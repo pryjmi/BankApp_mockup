b647135997b5b3a8dbd49c946778b33bd01f9ff3
JaroslavP <jaroslav.pryjmak@tul.cz>
2023-03-30 17:25:44 +0200

    balance, history, receive

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,6 @@
 import pytest
-import json
-import datetime
 import smtplib
-from flask import url_for
-from app.views import  get_date_to_use, get_exchange_rate, save_exchange_rate, send_email_pin
+from app.views import send_email_pin
 
 def test_login(client, monkeypatch):
     monkeypatch.setattr("app.views.send_email_pin", lambda email, pin: None)
@@ -56,40 +53,3 @@ def test_login_route_get(client):
     response = client.get("/login")
     assert response.status_code == 200
     assert b'Login' in response.data
-
-def test_get_date_to_use():
-    # weekend
-    weekend = datetime.datetime(2023, 3, 25) # saturday
-    expected_date_weekend = "24.03.2023" # nearest friday
-    assert get_date_to_use(weekend) == expected_date_weekend
-    # weekday
-    weekday = datetime.datetime(2023, 3, 22) # tuesday
-    expected_date_weekday = "22.03.2023" # same day
-    assert get_date_to_use(weekday) == expected_date_weekday
-
-def test_get_exchange_rate(tmp_path):
-    test_file = tmp_path / "exchange_rates_test.json"
-    get_exchange_rate(test_file)
-    
-    assert test_file.exists()
-    
-    with open(test_file, "r") as file:
-        exchange_rates = json.load(file)
-    
-    date_to_use = get_date_to_use()
-    assert date_to_use in exchange_rates
-
-def test_save_exchange_rate(tmp_path):
-    test_file = tmp_path / "exchange_rates_test.json"
-    test_date = "25.03.2023"
-    test_data = {}
-    
-    save_exchange_rate(test_date, test_data, test_file)
-    assert test_file.exists()
-    
-    with open(test_file, "r") as file:
-        data = json.load(file)
-    assert data
-    
-    assert test_date in data
-    assert data[test_date]
